b7bb9545536859029ff472e7eec9c7b9c95a4452
madankhadka2057 <madankhadka2057@gmail.com>
2024-07-11 10:19:58 +0545

    Add project files.

---
+++ DotNetDay3/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DotNetDay3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetDay3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Student> GetStudent()
+        {
+            List<Student> std = new List<Student>()
+            {
+                new Student{Id=1,Name="Madan",Address="Pyuthan"},
+                new Student{Id=2,Name="Badri",Address="Pyuthan"},
+                new Student{Id=4,Name="Hari",Address="Gulmi"},
+                new Student{Id=5,Name="Anita",Address="Butwal"},
+                new Student{Id=9,Name="Resham",Address="Rupandehi"},
+                new Student{Id=6,Name="Nabin",Address="Butwal"},
+                new Student{Id=8,Name="Durga",Address="Pyuthan"},
+
+            };
+            return std;
+        }
+        public IActionResult Index()
+        {
+            List < Student > StudentData= GetStudent();
+            //return Json(StudentData);
+            return View(StudentData);
+        }
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            List<Student> StudentData = GetStudent();
+            var std= StudentData.Where(x=>x.Id==id).First();
+            return Json(std);
+        }
+    }
+}
---
+++ DotNetDay3/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetDay3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute
+            (
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{ id?}"
+            );
+
+            app.Run();
+        }
+    }
+}
